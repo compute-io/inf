1e7cde558e29c5083e2876e08bd0f4c728590c36
kgryte <kgryte@gmail.com>
2015-07-19 14:11:36 -0700

    [UPDATE] lib, examples.

--- TODO.md
+++ TODO.md
@@ -1,4 +1,6 @@
 TODO
 ====
 
-
+1. Support `dstructs-ndarrays`.
+2. In README, ensure that examples includes specifying the sign (see examples.js)
+3. 
--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,51 @@
 'use strict';
 
-var foo = require( './../lib' );
+var inf = require( './../lib' ),
+	out;
+
+// ---
+// Plain arrays...
+
+// 1x10:
+out = inf( 10 );
+console.log( '1x10:' );
+console.log( out );
+console.log( '\n' );
+
+// 2x1x3:
+out = inf( [2,1,3] );
+console.log( '2x1x3:' );
+console.log( out );
+console.log( '\n' );
+
+// 5x5x5:
+out = inf( [5,5,5] );
+console.log( '5x5x5:' );
+console.log( out );
+console.log( '\n' );
+
+// 10x5x10x20:
+out = inf( [10,5,10,20] );
+console.log( '10x5x10x20:' );
+console.log( JSON.stringify( out ) );
+console.log( '\n' );
+// Note that infinity values are stringified as `null`.
+
+
+// ---
+// Typed arrays...
+out = inf( 10, {
+	'dtype': 'float32',
+	'sign': -1
+});
+console.log( 'Typed arrays:' );
+console.log( out );
+console.log( '\n' );
+
+
+// ---
+// Matrices...
+out = inf( [3,2], {
+	'dtype': 'float64'
+});
+console.log( 'Matrix: %s\n', out.toString() );
